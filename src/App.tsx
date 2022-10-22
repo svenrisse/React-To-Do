5d058dc61a3995835ae068561554c3c1e8113d1d
sven <svenrisse0@gmail.com>
2022-10-22 17:50:32 +0200

    added form and handleChange()

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Task from "./components/Task";
+import Add from "./components/Add";
 
 function App() {
 
@@ -9,12 +10,13 @@ function App() {
       <div className="navbar">
         <h1>Home</h1>
       </div>
-      <div className="addSection">
 
-      </div>
+      <Add />
+
       <div className="categories">
 
       </div>
+
       <div className="tasks">
         <Task title="Water the flowers" description="It's important to take care of nature"></Task>
       </div>
