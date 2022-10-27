7b6077ac230db9b0561ac8330627c3c4a223bc38
sven <svenrisse0@gmail.com>
2022-10-27 20:23:06 +0200

    styling: todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,11 +2,11 @@ function Todo(props) {
     return (
         <div className="todo">
             <div className="todo__info">
-                <span>{props.title}</span>
-                <span>{props.description}</span>
+                <span className="todo__title">{props.title}</span>
+                <span className="todo__description">{props.description}</span>
             </div>
-            <button id={props.id} onClick={props.handleClick}>
-                Remove
+            <button id={props.id} onClick={props.handleClick} className="todo__button">
+                -
             </button>
         </div>
     );
