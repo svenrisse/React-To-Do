98446bdd91857931c5ed0b6f34a0499b817c59d2
sven <svenrisse0@gmail.com>
2022-10-27 12:05:14 +0200

    feat: dynamic rendering of todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import Addsection from "./components/Addsection";
+import Todo from "./components/Todo";
 
 function App() {
     const [tasks, setTasks] = useState([]);
 
+    const toDos = tasks.map(task => {
+        return <Todo 
+                    key={tasks.indexOf(task)}
+                    title={task.title}
+                    description={task.description}
+                />
+    })
     return (
         <div className="app-container">
             <div className="topbar">
                 <Addsection tasks={tasks} setTasks={setTasks} />
             </div>
-            <div className="tasks-container"></div>
+            <div className="tasks-container">
+                {toDos}
+            </div>
         </div>
     );
 }
